1b6df15ecc5f48dfa0dc5661cf015ce4fa18fb1b
Роман <1deerain1@gmail.com>
2025-07-31 10:35:43 +0300

    update

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    NODE_ENV: string
+    DEV: boolean
+    PROD: boolean
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
