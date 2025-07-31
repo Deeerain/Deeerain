24da6248743c63fed0c95ad624c135f5cf38cb2e
Роман <1deerain1@gmail.com>
2025-07-31 10:35:43 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import { ImportMeta } from './src/vite-env'
 
 export default defineConfig({
   plugins: [vue()],
-  base: import.meta.env.PROD
+  base: import.meta.env
   ? '/Deeerain/'
   : '/',
 })
