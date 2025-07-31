b0544d53e12b27bbd4215bb91e3494ee50ba980e
Роман <1deerain1@gmail.com>
2025-07-31 10:06:08 +0300

    update ci-cd

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+<<<<<<< HEAD
   base: '/Deeerain/',
+=======
+  base: process.env.NODE_ENV === 'production' 
+  ? '/Deeerain/'
+  : '/',
+>>>>>>> 3f42d0f (update ci-cd)
 })
